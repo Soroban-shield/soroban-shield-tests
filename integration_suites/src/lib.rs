87bc66e8f43c84196cef0f9454bdd86eb865845b
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:16:06 +0100

    Register edge_renounce in integration lib

--- integration_suites/src/lib.rs
+++ integration_suites/src/lib.rs
@@ -1,6 +1,5 @@
-//! Multi-contract composability integration tests.
-
 pub mod access_multi_sig;
+pub mod edge_renounce;
 pub mod full_composition;
 pub mod ownable_pausable;
 pub mod rate_limiter_access;
