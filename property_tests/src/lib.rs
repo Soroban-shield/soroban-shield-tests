b3008595cb0d998086473c38c8a9c297fa9b1f07
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:14:52 +0100

    Add property tests module root

---
+++ property_tests/src/lib.rs
@@ -0,0 +1,7 @@
+//! Property-based invariant tests for Shield modules.
+
+pub mod access_control_props;
+pub mod multi_sig_props;
+pub mod ownable_props;
+pub mod pausable_props;
+pub mod rate_limiter_props;
