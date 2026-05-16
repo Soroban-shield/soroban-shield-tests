4b4cfeeb6fad857cabbcf0388fe52ffb50a42bf1
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:15:17 +0100

    Add RateLimiter + AccessControl integration suite

---
+++ integration_suites/src/rate_limiter_access.rs
@@ -0,0 +1,12 @@
+use soroban_sdk::{symbol_short, testutils::Address as _, Address, Env};
+use soroban_shield_contracts::contracts::{access_control, rate_limiter};
+
+#[test]
+fn admin_bypasses_rate_limit_config() {
+    let env = Env::default();
+    let admin = Address::generate(&env);
+    env.mock_all_auths();
+    access_control::grant_role(&env, &symbol_short!("admin"), &admin, &admin);
+    rate_limiter::configure(&env, 100, 60);
+    assert!(rate_limiter::check_and_record(&env, &admin, 0).is_ok());
+}
