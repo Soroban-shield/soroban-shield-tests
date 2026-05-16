7acb927dd38647e55b34bd6f1cb9de56cdb21a7a
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:15:08 +0100

    Add integration suites module root

---
+++ integration_suites/src/lib.rs
@@ -0,0 +1,7 @@
+//! Multi-contract composability integration tests.
+
+pub mod access_multi_sig;
+pub mod full_composition;
+pub mod ownable_pausable;
+pub mod rate_limiter_access;
+pub mod upgradeable_proxy;
