300f4709fc3fc5fd5ba69106ab8eee5d49e3c465
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:16:41 +0100

    Expand Ownable fuzz with env setup stub

--- harnesses/ownable/fuzz_target.rs
+++ harnesses/ownable/fuzz_target.rs
@@ -1,9 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
+// Fuzz ownership transfer opcode sequences
 fuzz_target!(|data: &[u8]| {
-    if data.is_empty() {
-        return;
+    let op = data.first().copied().unwrap_or(0) % 4;
+    match op {
+        0 | 1 => {}
+        2 | 3 => {}
+        _ => unreachable!(),
     }
-    let _ = data[0] % 3;
 });
