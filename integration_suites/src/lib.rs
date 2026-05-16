1d325483707ada25c975bfa6e58e2d833a68379e
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:17:02 +0100

    Register multi_sig_execute module

--- integration_suites/src/lib.rs
+++ integration_suites/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod access_multi_sig;
 pub mod edge_renounce;
 pub mod full_composition;
+pub mod multi_sig_execute;
 pub mod ownable_pausable;
 pub mod rate_limiter_access;
 pub mod upgradeable_proxy;
